c38546444198b26d1752aa04718fe3b03a3bf4ec
AstroAsh <astroaisha@gmail.com>
2017-02-26 21:12:45 +0000

    Backgrounds added and styling fine tuned

--- main.js
+++ main.js
@@ -18,46 +18,48 @@ $(document).ready(function() {
     }
     // function to create API
     function api(array) {
-        return "https://newsapi.org/v1/articles?source="+ randomElement(array)+"&apiKey=d647eb7abbc7414592db03051c5b2b5e";
+        return "https://newsapi.org/v1/articles?source=" + randomElement(array) + "&apiKey=d647eb7abbc7414592db03051c5b2b5e";
     }
     // name formatting
-    function removeFormatting(name){
-      return name.replace( /-/gi," ").replace(/\b\w/g, l => l.toUpperCase()).replace(/uk/gi,"UK").replace(/au/gi,"AU").replace(/cnn/gi,"CNN").replace(/cnbc/gi,"CNBC").replace(/bbc/gi,"BBC");
+    function removeFormatting(name) {
+        return name.replace(/-/gi, " ").replace(/\b\w/g, l => l.toUpperCase()).replace(/uk/gi, "UK").replace(/au/gi, "AU").replace(/cnn/gi, "CNN").replace(/cnbc/gi, "CNBC").replace(/bbc/gi, "BBC");
+    }
+
+    function removeSlash(url) {
+        if (url[url.length - 1] == "/") {
+            url = url.slice(0, -1);
+        }
+        return url;
     }
     // function for creating articles
-    function createArticle(data,paperID,articleID) {
-            console.log(data);
-            var paperName= removeFormatting(data.source);
-            $(paperID).text(paperName);
-            $(articleID).html()
-            for(i=0;i<3;i++){
-      $(articleID).append("<article><a href ="+ data.articles[i].url+"><h4>" + data.articles[i].title +"</h4>" + "<p>"+ data.articles[i].description+ "</p></a></article>")
+    function createArticle(data, paperID, articleID) {
+        // console.log(data);
+        var paperName = removeFormatting(data.source);
+
+        $(paperID).text(paperName);
+        $(articleID).html()
+        for (i = 0; i < 3; i++) {
+            var articleUrl = removeSlash(data.articles[i].url)
+            $(articleID).append("<article><a href =" + articleUrl + "><h4>" + data.articles[i].title + "</h4>" + "<p>" + data.articles[i].description + "</p></a></article>")
         }
     }
-    // console.log(api(leftArr));
-    // console.log(api(rightArr));
+
     // left data
     $.getJSON(api(leftArr), function(data) {
-      createArticle(data,"#leftPaper","#leftArt");
-//         var paperName= removeFormatting(data.source);
-//         $("#leftPaper").text(paperName);
-//         $("#leftArt").html()
-//         for(i=0;i<3;i++){
-//   $("#leftArt").append("<article><a href ="+ data.articles[i].url+"><h4>" + data.articles[i].title +"</h4>" + "<p>"+ data.articles[i].description+ "</p></a></article>")
-// }
+        createArticle(data, "#leftPaper", "#leftArt");
     });
     // Center left data
     $.getJSON(api(cenLeftArr), function(data) {
-        createArticle(data,"#cenLeftPaper","#cenLeftArt");
+        createArticle(data, "#cenLeftPaper", "#cenLeftArt");
     });
 
     // Center Right data
     $.getJSON(api(cenRightArr), function(data) {
-        createArticle(data,"#cenRightPaper","#cenRightArt");
+        createArticle(data, "#cenRightPaper", "#cenRightArt");
     });
     // Right data
     $.getJSON(api(rightArr), function(data) {
-        createArticle(data,"#rightPaper","#rightArt");
+        createArticle(data, "#rightPaper", "#rightArt");
 
     });
 
